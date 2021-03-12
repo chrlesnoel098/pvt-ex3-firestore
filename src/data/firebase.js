5389ada1961875750103ec791425ad36202fb9c7
charlesNoel <charlesnoel098@gmail.com>
2021-03-12 15:59:45 -0500

    supprimer config firestore

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -1,17 +1,6 @@
 import fbAppli from 'firebase/app';
 import 'firebase/firestore';
 
-/******* Ex#3 - Étape B ********************************/ 
-// Ajouter votre objet de configuration Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyAgAp5tFRToZSLAeI-5M8rBFe03_bqVQMQ",
-  authDomain: "cn-react.firebaseapp.com",
-  projectId: "cn-react",
-  storageBucket: "cn-react.appspot.com",
-  messagingSenderId: "1065458615900",
-  appId: "1:1065458615900:web:24750a6ab98b7a9a38c899",
-  measurementId: "G-Q3ZNK2873W"
-};
 
 
 // Initialiser Firebase
